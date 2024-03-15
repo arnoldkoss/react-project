b9f78204fafca75adb2e9bed531bcb7f095d384c
arnold buiuca <arnoldbuiuka@gmail.com>
2024-03-15 14:22:51 +0000

    add a callback function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from "./components/StatefulGreetingWithCallback";
 
 function App() {
   return (
     <div className="App">
+      <StatefulGreetingWithCallback />
+      <StatefulGreeting greeting="Im a statefull class component" name="Mike"/>
       <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Nice to meet you!!" name="Mike" age="32"/>
     </div>
