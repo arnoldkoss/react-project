f038fdafd14c71a088aa78c9eddc934e94ffdf53
arnold buiuca <arnoldbuiuka@gmail.com>
2024-03-16 12:10:02 +0000

    create a new component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,12 @@ import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from "./components/StatefulGreetingWithCallback";
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
+      <StatefulGreetingWithPrevState />
       <StatefulGreetingWithCallback />
       <StatefulGreeting greeting="Im a statefull class component" name="Mike"/>
       <FunctionalGreeting />
