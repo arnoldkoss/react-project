2c55156eb8020aedffa055333959bf032998bb91
arnold buiuca <arnoldbuiuka@gmail.com>
2024-03-17 12:47:58 +0000

    create different components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithCallback from "./components/StatefulGreetingWithCallback";
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import MethodsAsPropsChild from './components/MethodsAsPropsChild';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreetingWithPrevState />
-      <StatefulGreetingWithCallback />
-      <StatefulGreeting greeting="Im a statefull class component" name="Mike"/>
-      <FunctionalGreeting />
-      <FunctionalGreetingWithProps greeting="Nice to meet you!!" name="Mike" age="32"/>
+      <MethodsAsPropsChild />
+      <MethodsAsPropsParent />
     </div>
   );
 }
--- my-app/src/components/FunctionalGreetingWithProps.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-const FunctionalGreetingWithProps = (props) => {
-    console.log(props);
-    return <h1>Hello there!! {props.name}, I see your {props.age} years old, {props.greeting}</h1>;
-}
-
-export default FunctionalGreetingWithProps;
