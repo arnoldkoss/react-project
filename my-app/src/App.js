624fa95f341f82ecbc3052ebdd19cad59d197072
arnold buiuca <arnoldbuiuka@gmail.com>
2024-03-22 12:27:23 +0000

    more components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import SearchBar from './components/SearchBar';
+import UseStateWithArrays from './components/UseStateWithArrays';
 
 
 
 function App() {
   return (
     <div className="App">
-      <SearchBar />
+      <UseStateWithArrays />
     </div>
   );
 }
